6d23e4fad7c7eb6462b7073948d405b3d0c8da0c
Tselikina Anna <111611506+kenikts@users.noreply.github.com>
2022-12-04 17:18:42 +0200

    Add files via upload

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,7 @@
 alert("Hello, dear user!");
 alert("let's get to know each other better");
 var city = prompt('Where are you from?');
+alert(city + '?')
 alert('Cool!');
 document.write(city);
 console.log(city)
